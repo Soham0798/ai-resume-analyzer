234a4324c1f6255ffbd477860e534e6e252ca5b8
Soham0798 <sohambz.patwardhan@gmail.com>
2026-07-31 11:50:34 +0530

    Initial changes

---
+++ app/routes/Auth.tsx
@@ -0,0 +1,18 @@
+export const meta = () => ([
+    {title: 'ResumeAnalyse | Auth'},
+    {name: 'description', content: 'Log into your account'},
+])
+
+const Auth = () => {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className="gradient-border shadow-lg">
+            <section className=" flex-col gap-8 bg-white">
+
+            </section>
+        </div>
+    </main>
+  )
+}
+
+export default Auth
